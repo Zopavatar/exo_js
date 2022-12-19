5322f24851854384f65e6e40d4dc8f04b73af11a
unknown <Charlotte Landuyt>
2022-12-19 11:35:12 +0100

    exo1

---
+++ public/js/javascript.js
@@ -0,0 +1,12 @@
+//exo1
+
+let prenom = "Charlotte";
+console.log(prenom);
+
+let age = 24;
+console.log(age);
+
+let presentation = `Bonjour, je m'appelle ${prenom} et j'ai ${age} ans.`;
+
+console.log(presentation);
+alert(presentation);
